65236b667318e275a6883f7ea54aec872533f116
gowadiuk <gabriel.fffernandes@gmail.com>
2026-01-02 23:33:54 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -13,7 +13,9 @@ namespace SalesWebMvc.Data
             : base(options)
         {
         }
-
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
     }
 }
